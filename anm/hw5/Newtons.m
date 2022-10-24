484047384cbba34f872f449c29d3b61a62664b18
Josh <josh@mjbrowns.com>
2022-10-24 18:53:31 -0400

    feat: add other problems

--- anm/hw5/Newtons.m
+++ anm/hw5/Newtons.m
@@ -1,6 +1,11 @@
-function x = Newtons(fx, fpx, tol, x0)
+function x = Newtons(fx, fpx, tol, x0, r, p)
 %NEWTONS Summary of this function goes here
 %   Determins an approximation to the root given fx, fpx, a tolorant and x0
+    if nargin == 6
+        useR = true;
+    else
+        useR = false;
+    end
     x = x0;
     xlast = 0;
     i = 0;
@@ -9,6 +14,10 @@ function x = Newtons(fx, fpx, tol, x0)
         x = x - fx(x)/fpx(x);
         disp("iteration " + i + ": " + x)
         i = i + 1;
+        if useR == true
+            ratio = abs(x-r)/abs(xlast-r).^p;
+            disp("Ratio: " + ratio)
+        end
     end
 end
 
